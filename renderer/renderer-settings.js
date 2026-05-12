76346fed211b6d499a8864e34444546849554593
cenguanming <975402925@qq.com>
2026-05-12 15:01:58 +0800

    fix: remove useless comment

--- renderer/renderer-settings.js
+++ renderer/renderer-settings.js
@@ -1,3 +1,3 @@
 export const RENDERER_SETTINGS_KEY = Symbol('RENDERER_SETTINGS_KEY')
 
-export const APPLY_DEFAULT_PROPS_KEY = Symbol('APPLY_DEFAULT_PROPS_KEY') // 暂不在genui-sdk-vue中对外暴露
+export const APPLY_DEFAULT_PROPS_KEY = Symbol('APPLY_DEFAULT_PROPS_KEY')
