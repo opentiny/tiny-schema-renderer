116602358f4202f04f31ef87645b5923fe178c82
yy <yaoyun_bigger@outlook.com>
2026-05-18 19:33:10 +0800

    refactor: clear out defaultMaterials by removing unused TinyChart imports

--- renderer/useCustomSetting.ts
+++ renderer/useCustomSetting.ts
@@ -1,26 +1,11 @@
 import type { Component } from 'vue'
-import * as TinyVue from '@opentiny/vue'
-import TinyChartPie from '@opentiny/vue-chart-pie'
-import TinyChartRadar from '@opentiny/vue-chart-radar'
-import TinyChartBar from '@opentiny/vue-chart-bar'
-import TinyChartHistogram from '@opentiny/vue-chart-histogram'
-import TinyChartLine from '@opentiny/vue-chart-line'
-import TinyChartRing from '@opentiny/vue-chart-ring'
 
 export interface IRendererSettings {
   Function?: FunctionConstructor
   materials?: Record<string, Component>
 }
 
-const defaultMaterials: Record<string, Component> = {
-  ...(TinyVue as unknown as Record<string, Component>),
-  TinyChartPie,
-  TinyChartRadar,
-  TinyChartBar,
-  TinyChartHistogram,
-  TinyChartLine,
-  TinyChartRing
-}
+const defaultMaterials: Record<string, Component> = {}
 
 export const DEFAULT_RENDERER_SETTINGS: IRendererSettings = {
   Function: Function,
